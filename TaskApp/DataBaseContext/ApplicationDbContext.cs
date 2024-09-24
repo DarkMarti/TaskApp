f7ae298cfeaf413ca7af9aa8ea087ad84c5eba88
Marti <dam009.2019@gmail.com>
2024-09-24 21:05:02 +0200

    create entities Category, Task and DBContext

---
+++ TaskApp/DataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskApp.Models;
+
+namespace TaskApp.DataBaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Task> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ TaskApp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public string Abbreviation { get; set; } = "";
+
+        public string Color { get; set; } = "";
+
+        public List<Task>? Tasks { get; set; } = new List<Task>();
+    }
+}
